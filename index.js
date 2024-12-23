adb109e39c3e721f0507d920c3366b479901f182
Jake Rogers <87666498+Rakejogers@users.noreply.github.com>
2024-12-23 00:32:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const fastify = Fastify();
 fastify.register(fastifyFormBody);
 fastify.register(fastifyWs);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 3000;
 
 // Root route for health check
 fastify.get("/", async (_, reply) => {
